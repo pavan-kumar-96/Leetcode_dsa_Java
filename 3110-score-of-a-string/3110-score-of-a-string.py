0a337f1f72648a5336d1173b4b76f930b9a81b4e
Pavan Kumar Gurugubelli <pondhupavan@gmail.com>
2025-12-28 14:34:20 +0530

    Time: 25 ms (1.62%), Space: 16.4 MB (100%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.py
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -3,5 +3,5 @@ class Solution:
         ans = 0
         for i in range(len(s)-1):
              temp=abs(ord(s[i])-ord(s[i+1]))
-             ans += temp
+             ans =ans+temp
         return ans
