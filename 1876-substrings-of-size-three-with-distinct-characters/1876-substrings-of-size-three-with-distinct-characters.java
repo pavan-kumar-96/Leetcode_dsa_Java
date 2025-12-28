2964af466a774fca5855548a0553d3749e403597
Pavan Kumar Gurugubelli <pondhupavan@gmail.com>
2025-12-28 15:08:23 +0530

    Time: 3 ms (32.69%), Space: 42.2 MB (93.07%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,23 +1,23 @@
 class Solution {
-    public boolean fun(String s, int k) {
-        HashSet<Character> hs = new HashSet<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            hs.add(ch);
-        }
-        if (hs.size() == 3) {
-            return true;
-        } else {
-            return false;
-        }
-    }
     public int countGoodSubstrings(String s) {
         int n = s.length();
         int ans = 0;
-        int k = 3;
-        for (int i = 0; i < n - k + 1; i++) {
-            boolean temp = fun(s.substring(i, i + k), k);
-            if (temp) {
+        int l = 0;
+        int sl = 3;
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int r = 0; r < n; r++) {
+            char ch = s.charAt(r);
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+            if (r - l == sl) {
+                char tch = s.charAt(l);
+                hm.put(tch, hm.get(tch) - 1);
+                if (hm.get(tch) == 0) {
+                    hm.remove(tch);
+                }
+                l++;
+            }
+
+            if (hm.size() == sl) {
                 ans += 1;
             }
         }
