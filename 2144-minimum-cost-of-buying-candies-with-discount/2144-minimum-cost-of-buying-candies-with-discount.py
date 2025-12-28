5bf3e72d6584063c7300634dd9e69244d1550c29
Pavan Kumar Gurugubelli <pondhupavan@gmail.com>
2025-12-28 15:15:16 +0530

    Time: 0 ms (100%), Space: 12.6 MB (5.41%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def minimumCost(self, cost):
         cost.sort()
-        n=len(cost)
         ans=0
         took=0
-        for i in range(n-1,-1,-1):
+        for i in range(len(cost)-1,-1,-1):
             if took==2:
                 took=0
             else:
