08021a22a6210678c8995181c0176a9335e94232
Pavan Kumar Gurugubelli <pondhupavan@gmail.com>
2025-12-28 14:42:44 +0530

    Time: 42 ms (5.67%), Space: 16.6 MB (100%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -2,9 +2,14 @@ class Solution:
     def maxDistance(self, colors: List[int]) -> int:
         ans=0
         n=len(colors)
+        for i in range(n-1,-1,-1):
+            if colors[i]!=colors[0]:
+                temp=abs(i)
+                ans=max(ans,temp)
+                break
         for i in range(n):
-            for j in range(i+1,n):
-                if colors[i]!=colors[j]:
-                    temp=abs(i-j)
-                    ans=max(ans,temp)
+            if colors[i]!=colors[n-1]:
+                temp=abs(n-i-1)
+                ans=max(ans,temp)
+                break
         return ans
