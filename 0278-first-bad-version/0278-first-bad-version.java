cacf5c65defd4fd37865119cd72d585666e8ad8e
Pavan Kumar Gurugubelli <pondhupavan@gmail.com>
2025-12-28 15:28:31 +0530

    Time: 25 ms (9.51%), Space: 40.1 MB (99.64%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,8 +3,8 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int l = 0;
-        int r = n - 1;
+        int l = 1;
+        int r = n;
         while (l <= r) {
             int mid = l + (r - l) / 2;
             if (isBadVersion(mid)) {
